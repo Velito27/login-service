b19a29b76cd36201a77df176bbe75ae2a7f904d2
developer <cordero.velito.27@gmail.com>
2026-03-25 09:51:05 -0500

    controller created

--- src/main/java/com/justin/loginservice/controller/LoginController.java
+++ src/main/java/com/justin/loginservice/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.justin.loginservice.controller;
 
 
+import com.justin.loginservice.entity.Login;
 import com.justin.loginservice.service.LoginService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
@@ -13,6 +16,21 @@ public class LoginController {
 
     public LoginController(LoginService loginService){
         this.loginService = loginService;
+    }
+
+    @PostMapping
+    public Login createLogin(@RequestBody Login login){
+        return loginService.saveLogin(login);
+    }
+
+    @GetMapping
+    public List<Login> getAllLogins(){
+        return loginService.getAllLogins();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Login> getLoginById(@PathVariable Long id){
+        return loginService.getLoginById(id);
     }
 
 }
